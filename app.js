13d8b480a743bfea9102d9fbe4533243ac687f28
LinhHoang2812 <zoeylinh2812@gmail.com>
2023-05-09 22:11:01 +0200

    proxy cors

--- app.js
+++ app.js
@@ -2,22 +2,29 @@ const btn = document.querySelector(".test-btn");
 btn.addEventListener("click", click);
 
 function click() {
-  const result = fetch("https://azienda2.staging.re2n.app/api/auth/sessions", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      Email: "test@re2n.com",
-      Password: "Testre2n12342023!",
-    }),
-  }).then(function (response) {
+  const result = fetch(
+    "https://proxy.cors.sh/https://azienda2.staging.re2n.app/api/auth/sessions'",
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "x-cors-api-key": "temp_4cc195e07f15a84cb4fe6faaab736d78",
+      },
+      body: JSON.stringify({
+        Email: "test@re2n.com",
+        Password: "Testre2n12342023!",
+      }),
+    }
+  ).then(function (response) {
     if (response.status === 200) {
       fetch(
-        "https://azienda2.staging.re2n.app/api/reporting/gri/reports/report_list",
+        "https://proxy.cors.sh/https://azienda2.staging.re2n.app/api/reporting/gri/reports/report_list",
         {
           method: "GET",
+          headers: {
+            "x-cors-api-key": "temp_4cc195e07f15a84cb4fe6faaab736d78",
+          },
         }
       )
         .then(function (response) {
